456f2bd6fced68057d80bc12677e96568ed6c0c8
Mario <marioodevv@gmail.com>
2024-10-18 16:49:09 +0200

    Lógica funcionando

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ let caja_titulo = document.querySelector("#caja-titulo");
 
 function storageElements() {
     btn.addEventListener("click", ((e) => {
-        e.preventDefault()
+        // e.preventDefault()
         if (titulo.value !== "" && subtitulo.value !== "" && descripcion.value !== "") {
 
             let contador = localStorage.length;
@@ -24,14 +24,14 @@ function storageElements() {
 
 
 
-            
+
 
         } else {
-            if(popUp.classList.contains("hidden")){
+            if (popUp.classList.contains("hidden")) {
                 popUp.classList.remove("hidden");
-                setTimeout((()=>{
+                setTimeout((() => {
                     popUp.classList.add("hidden");
-                }),2000)
+                }), 2000)
             };
         };
 
@@ -41,15 +41,51 @@ function storageElements() {
 };
 
 
-function getElements ( ) {
+function getElements() {
     let contador = localStorage.length;
+    let caja_contenido = document.querySelector("#caja1");
+    let form = document.querySelector("#formulario");
+    console.log("Vacio");     // caja_contenido.classList.contains("flex");     caja_contenido.classList.remove("flex");    caja_contenido.classList.add("hidden"); } else {     caja_contenido.classList.remove("hidden");     caja_contenido.classList.add("flex"); }
 
     for (let i = 1; i <= contador; i++) {
         let dataStorage = localStorage.getItem(`info ${i}`);
 
-        if(dataStorage) {
+        if (dataStorage) {
             let legible = JSON.parse(dataStorage);
-            console.log(legible)
+            
+            caja_contenido.innerHTML += `<div id="art1" class="
+                h-40
+                py-3
+                px-5
+                w-1/4
+                md:w-full
+              bg-blue-400
+                overflow-hidden
+                text-center
+                flex 
+                justify-center
+                flex-wrap
+                flex-col
+                flex-grow
+                content-center
+
+                ">
+                    <h1 id="caja-titulo" class="text-2xl font-bold text-gray-800 text-wrap h-1/4 w-full">${legible.titulos}</h1>
+                    <h3 class=" max-h-7 truncate">${legible.subtitulos}</h3>
+                    <button class="h-1/4
+                     w-full
+                   bg-blue-50 border
+                   border-black
+                      rounded-xl">
+                        Ver
+                    </button>
+                </div>`
+
+                //     let crear_sec = document.createElement("section");
+                // crear_sec.className = "h-auto px-2 py-1 flex justify-around flex-wrap gap-1";
+
+
+
         }
     }
 }

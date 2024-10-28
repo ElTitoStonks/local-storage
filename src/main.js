97ac1185fef702f8560cf53382c0f0cb1f885317
Mario <marioodevv@gmail.com>
2024-10-28 20:39:44 +0100

    finish-app

--- src/main.js
+++ src/main.js
@@ -4,14 +4,9 @@ let titulo = document.querySelector("#titulo");
 let subtitulo = document.querySelector("#subtitulo");
 let descripcion = document.querySelector("#descripcion");
 let btn = document.querySelector("#enviar");
-let popUp = document.querySelector("#pop");
-
-let caja_titulo = document.querySelector(".caja-titulo");
 
 function storageElements() {
-    // Function to store the elements in the local storage
     btn.addEventListener("click", (e) => {
-        // e.preventDefault();
         if (titulo.value !== "" && subtitulo.value !== "" && descripcion.value !== "") {
             let contador = localStorage.length;
             const storage = {
@@ -21,69 +16,87 @@ function storageElements() {
             };
 
             localStorage.setItem(`info ${contador + 1}`, JSON.stringify(storage));
-
-        };
+        }
     });
 }
 
-// function to get the elements from the local storage
 function getElements() {
     let contador = localStorage.length;
     let caja_contenido = document.querySelector("#caja1");
     for (let i = 1; i <= contador; i++) {
         let dataStorage = localStorage.getItem(`info ${i}`);
-        // condition to check if there is any data in the local storage
         if (dataStorage) {
             let legible = JSON.parse(dataStorage);
-            // if exists the data in local storage, the data is shown in the screen
             caja_contenido.innerHTML += `
                 <div class="
-                    art
-                    h-40
-                    py-3
-                    px-5
-                    w-full
-                    bg-blue-400
-                    overflow-hidden
-                    text-center
-                    flex 
-                    justify-center
-                    flex-wrap
-                    flex-col
-                    content-center
+                    art h-40 py-3 px-5 w-full bg-blue-400 overflow-hidden text-center flex justify-center flex-wrap flex-col content-center relative
                 ">
                     <h1 class="caja-titulo text-2xl font-bold text-gray-800 text-wrap h-1/4 w-full">${legible.titulos}</h1>
-                    <h3 class=" max-h-7 truncate">${legible.subtitulos}</h3>
+                    <h3 class="max-h-7 truncate">${legible.subtitulos}</h3>
                     <button id="btn_pop_up${i}" class="h-1/4 w-full bg-blue-50 border border-black rounded-xl">
-                        Ver<span id="pop_btn${i}" class="pop hidden bg-blue-300 fixed top-14 md:left-1/3 left-4 w-[90dvw] md:w-1/3 h-[60dvh] border border-black rounded-3xl shadow-lg shadow-gray-600">
-                        <img src="./cross.svg" alt="cerrar" class="img__cross${i} w-auto h-8 absolute right-5 top-3 cursor-pointer>
-                        <h1 class="text-2xl font-bold">${legible.titulos}</h1>
-                        <h3>${legible.subtitulos}</h3>
-                        <p>${legible.descripciones}</p>
+                        Ver
+                        <span id="pop_btn${i}" class="pop__hidden ">
+                            <img src="./cross.svg" alt="cerrar" class="img__cross${i} w-auto h-8 absolute right-5 top-3 cursor-pointer">
+                            <div class="text-2xl font-bold my-5 px-8">
+                            <h1>${legible.titulos}</h1>
+                            </div>
+                            <div class="h-auto w-full bg-blue-50 opacity-70 px-5 py-5 text-left text-xl mb-7 text-black/80">
+                                <h3>${legible.subtitulos}</h3>
+                            </div>
+                            <div class="bg-white h-44 md:h-72 mb-10 text-left px-5 py-5 text-xl text-gray-600">
+                            <p>${legible.descripciones}</p>
+                            </div>
                         </span>
                     </button>
                 </div>`;
         }
     }
 
-    // asssing the event listener to the pop-up button
     for (let i = 1; i <= contador; i++) {
         let btnPopUp = document.querySelector(`#btn_pop_up${i}`);
         let popUp = document.querySelector(`#pop_btn${i}`);
         let cross = document.querySelector(`.img__cross${i}`);
 
         btnPopUp.addEventListener("click", () => {
-            if (popUp.classList.contains("hidden")) {
-                popUp.classList.remove("hidden");
-            } else if (!popUp.classList.contains("hidden")) {
-                cross.addEventListener("click", () => {
-                    popUp.classList.add("");//add custom class to hide the pop-up
-                    // popUp.classList.remove("hidden");
-                });
-            }
+            popUp.classList.add("pop");
+            popUp.classList.remove("pop__hidden");
+        });
+
+        cross.addEventListener("click", (e) => {
+            e.stopPropagation();  // Evita que el evento cierre se dispare al hacer clic en el botón Ver
+            popUp.classList.add("pop__hidden");
+            popUp.classList.remove("pop");
         });
     }
 }
 
+function deleteElements() {
+    //Get elements from local storage
+    let box = document.querySelectorAll(".art");
+    let elementss = box.length;
+
+    console.log(elementss);
+
+    box.forEach(element => {
+        element.innerHTML += `
+            <span class="absolute w-8 right-3 top-2 cursor-pointer transition-all  ease-in-out duration-700 hover:rotate-180">
+            <img src="./cross.svg" class="cross-c">
+            </span>
+        `;
+    });
+
+    let cross = document.querySelectorAll(".cross-c");
+
+    cross.forEach(element => { 
+        element.addEventListener("click", () => {
+            localStorage.removeItem(`info ${elementss}`);
+            window.location.reload();
+        });
+    });
+
+};
+
+
 getElements();
 storageElements();
+deleteElements();
